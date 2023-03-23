5f2fc702369739c7569b29596e7850471d634c0d
JDuque <diegoduqqe@gmail.com>
2023-03-23 10:47:44 -0500

    envío y base estrcutura core

---
+++ PackageDelivery.Repository.Contracts/DbModels/Core/SendDbModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PackageDelivery.Repository.Contracts.DbModels.Core
+{
+    public class HistoryDbModel
+    {
+        public long Id { get; set; }
+
+        public DateTime SendDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public long IdSender { get; set; }
+
+        public long idAddress { get; set; }
+
+        public long IdPackage { get; set; }
+
+        public long idState { get; set; }
+
+        public long IdTransportType { get; set; }
+    }
+}
---
+++ PackageDelivery.Repository.Implementation/Mappers/Core/SendRepositoryMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageDelivery.Repository.Implementation.Mappers.Core
+{
+    internal class SendRepositoryMapper
+    {
+    }
+}
