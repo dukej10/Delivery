6e3e8da105a6a4b6aee861718e54aad60b654001
JDuque <diegoduqqe@gmail.com>
2023-03-23 11:30:57 -0500

    mapper municipio y bodega

---
+++ PackageDelivery.Repository.Contracts/DbModels/Core/StoreDbModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageDelivery.Repository.Contracts.DbModels.Core
+{
+    public class StoreDbModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string  Code { get; set; }
+        public string Adddress { get; set; }
+
+        public string Latitude  { get; set; }
+
+        public string Longitude { get; set; }
+
+        public long IdMunicipality { get; set; }
+    }
+}
+    
