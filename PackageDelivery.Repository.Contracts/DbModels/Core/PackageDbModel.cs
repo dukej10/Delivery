00916138fd65faa517bb5fea28fb10329673f04c
MiguelCuellarR <miguel.1701823659@ucaldas.edu.co>
2023-03-23 11:52:01 -0500

    DbModel y Mapper de Paquete y Office

---
+++ PackageDelivery.Repository.Contracts/DbModels/Core/PackageDbModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PackageDelivery.Repository.Contracts.DbModels.Core
+{
+    public class PackageDbModel
+    {
+        public long Id { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Width { get; set; }
+
+        public Nullable<long> IdOffice { get; set; }
+    }
+}
---
+++ PackageDelivery.Repository.Implementation/Mappers/Core/PackageRepositoryMapper.cs
@@ -0,0 +1,56 @@
+﻿using PackageDelivery.Repository.Contracts.DbModels.Core;
+using PackageDelivery.Repository.Implementation.DataModel;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Repository.Implementation.Mappers.Core
+{
+    public class PackageRepositoryMapper : DbModelMapperBase<PackageDbModel, paquete>
+    {
+        public override PackageDbModel DatabaseToDbModelMapper(paquete input)
+        {
+            return new PackageDbModel()
+            {
+                Id = input.id,
+                Weight = input.peso,
+                Height = input.altura,
+                Depth = input.profundidad,
+                Width = input.ancho,
+                IdOffice = input.idOficina
+            };
+        }
+
+        public override IEnumerable<PackageDbModel> DatabaseToDbModelMapper(IEnumerable<paquete> input)
+        {
+            IList<PackageDbModel> list = new List<PackageDbModel>();
+            foreach (var item in input)
+            {
+                list.Add(this.DatabaseToDbModelMapper(item));
+            }
+            return list;
+        }
+
+        public override paquete DbModelToDatabaseMapper(PackageDbModel input)
+        {
+            return new paquete()
+            {
+                id = input.Id,
+                peso = input.Weight,
+                altura = input.Height,
+                profundidad = input.Depth,
+                ancho = input.Width,
+                idOficina = input.IdOffice
+
+            };
+        }
+
+        public override IEnumerable<paquete> DbModelToDatabaseMapper(IEnumerable<PackageDbModel> input)
+        {
+            IList<paquete> list = new List<paquete>();
+            foreach (var item in input)
+            {
+                list.Add(this.DbModelToDatabaseMapper(item));
+            }
+            return list;
+        }
+    }
+}
