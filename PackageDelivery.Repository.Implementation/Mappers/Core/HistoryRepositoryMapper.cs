ac7866d05d0162a7256597a80a9bfebc5370d47a
Camilo-Caldas <juan.1701812099@ucaldas.edu.co>
2023-03-30 09:14:27 -0500

    Mapper Vehiculo e Historial

---
+++ PackageDelivery.Repository.Implementation/Mappers/Core/HistoryRepositoryMapper.cs
@@ -0,0 +1,63 @@
+﻿//HistoryRepositoryMapper
+using PackageDelivery.Repository.Contracts.DbModels.Core;
+using PackageDelivery.Repository.Contracts.DbModels.Parameters;
+using PackageDelivery.Repository.Implementation.DataModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageDelivery.Repository.Implementation.Mappers.Core
+{
+    internal class HistoryRepositoryMapper : DbModelMapperBase<HistoryDbModel, historial>
+    {
+        public override HistoryDbModel DatabaseToDbModelMapper(historial input)
+        {
+            return new HistoryDbModel()
+            {
+                Id = input.id,
+                AdmissionDate = input.fechaIngreso,
+                DepartureDate = input.fechaSalida,
+                Description = input.descripcion,
+                IdPackage = input.idPaquete,
+                IdStore = input.idBodega
+            };
+        }
+
+        public override IEnumerable<HistoryDbModel> DatabaseToDbModelMapper(IEnumerable<historial> input)
+        {
+            IList<HistoryDbModel> list = new List<HistoryDbModel>();
+            foreach (var item in input)
+            {
+                list.Add(this.DatabaseToDbModelMapper(item));
+            }
+            return list;
+        }
+
+        public override historial DbModelToDatabaseMapper(HistoryDbModel input)
+        {
+            return new historial()
+            {
+                id = input.Id,
+                fechaIngreso = input.AdmissionDate,
+                fechaSalida = input.DepartureDate,
+                descripcion = input.Description,
+                idPaquete = input.IdPackage,
+                idBodega = input.IdStore
+            };
+        }
+
+        public override IEnumerable<historial> DbModelToDatabaseMapper(IEnumerable<HistoryDbModel> input)
+        {
+            IList<historial> list = new List<historial>();
+            foreach (var item in input)
+            {
+                list.Add(this.DbModelToDatabaseMapper(item));
+            }
+            return list;
+        }
+    }
+}
---
+++ PackageDelivery.Repository.Implementation/Mappers/Core/VehicleRepositoryMapper.cs
@@ -0,0 +1,56 @@
+﻿using PackageDelivery.Repository.Contracts.DbModels.Core;
+using PackageDelivery.Repository.Contracts.DbModels.Parameters;
+using PackageDelivery.Repository.Implementation.DataModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageDelivery.Repository.Implementation.Mappers.Parameters
+{
+    internal class VehicleRepositoryMapper : DbModelMapperBase<VehicleDbModel, vehiculo>
+    {
+        public override VehicleDbModel DatabaseToDbModelMapper(vehiculo input)
+        {
+            return new VehicleDbModel()
+            {
+                Id = input.id, 
+                plate = input.placa,
+                IdTransportType = input.idTipoTransporte
+            };
+        }
+
+        public override IEnumerable<VehicleDbModel> DatabaseToDbModelMapper(IEnumerable<vehiculo> input)
+        {
+            IList<VehicleDbModel> list = new List<VehicleDbModel>();
+            foreach (var item in input)
+            {
+                list.Add(this.DatabaseToDbModelMapper(item));
+            }
+            return list;
+        }
+
+        public override vehiculo DbModelToDatabaseMapper(VehicleDbModel input)
+        {
+            return new vehiculo()
+            {
+                id = input.Id, 
+                placa = input.plate, 
+                idTipoTransporte = input.IdTransportType
+            };
+        }
+
+        public override IEnumerable<vehiculo> DbModelToDatabaseMapper(IEnumerable<VehicleDbModel> input)
+        {
+            IList<vehiculo> list = new List<vehiculo>();
+            foreach (var item in input)
+            {
+                list.Add(this.DbModelToDatabaseMapper(item));
+            }
+            return list;
+        }
+    }
+}
