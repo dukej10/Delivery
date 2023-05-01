17849b8d00df626a40710da210a4f9b207c82c09
JDuque <diegoduqqe@gmail.com>
2023-05-01 11:53:50 -0500

    quitar import que no se usan

--- PackageDelivery.Application.Contracts/Interfaces/Parameters/IDocumentTypeApplication.cs
+++ PackageDelivery.Application.Contracts/Interfaces/Parameters/IDocumentTypeApplication.cs
@@ -1,9 +1,5 @@
 ﻿using PackageDelivery.Application.Contracts.DTO;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PackageDelivery.Application.Contracts.Interfaces.Parameters
 {
