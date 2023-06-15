37571f37c713cabdcd31d329740479bd94ad5d6a
Camilo-Caldas <juan.1701812099@ucaldas.edu.co>
2023-06-15 03:38:20 -0500

    Model

---
+++ PackageDelivery.GUI/Models/HistoryModels.cs
@@ -0,0 +1,55 @@
+﻿//HistoryModels
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace PackageDelivery.GUI.Models
+{
+    public class IndexViewModel
+    {
+
+        public dateTime admissionDate { get; set; } //
+        public dateTime departureDate { get; set; }//
+        public string descripcion { get; set; }//
+
+    }
+
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+
+
+
+    public class VerifyadmissionDateViewModel
+    {
+        
+        [Required]
+        [admissionDate]
+        [Display(Name = "Fecha de Ingreso")]
+        public dateTime admissionDate { get; set; }
+
+        [Required]
+        [Display(Name = "fecha de salida")]
+        public dateTime departureDate { get; set; }
+
+        [Required]
+        [Display(Name = "fecha de salida")]
+        public string descripcion { get; set; }
+
+    }
+
+    public class ConfiguredescripcionViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+    }
+}
---
+++ PackageDelivery.GUI/Models/VehicleModels.cs
@@ -0,0 +1,41 @@
+﻿//VehicleModels
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace PackageDelivery.GUI.Models
+{
+    public class VehicleModels
+    {
+        public string Plaque { get; set; }
+    }
+
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+
+
+
+    public class VerifyPlaqueViewModel
+    {
+        [Required]
+        [Plaque]
+        [Display(Plaque = "Número de placa")]
+        public string Plaque { get; set; }
+    }
+
+    public class ConfigureTwoFactorViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+    }
+}
+
